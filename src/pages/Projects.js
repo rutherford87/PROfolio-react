ed24fc9b362487c00ce28631f7c122b8c9e36a4f
Tyler Rutherford <rutherford87@gmail.com>
2021-05-24 21:57:28 -0500

    updated cards on portfolio page, updated jumbotron

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,65 @@
+import {Card, Row, Container, Button} from 'react-bootstrap';
 
-import Container from "../components/Container";
 
 
 function Search() {
-            return (
-                <div className="hero text-center">
-                    <Container style={{ minHeight: "100vh" }}>
-                        <h1 className="text-center">Pupster</h1>
-                        <h2> Search here!!</h2>
-                    </Container>
-                </div>
-            );
-    }
+    return (
+        <div>
+            <Container className="d-flex justify-content-center" fluid>
+                <Card className="mb-3" style={{ color: "#000" }} style={{ width: '36rem' }}>
+                    <Card.Img src="../assets/FreelancR"/>
+
+                    <Card.Body>
+                            <Card.Title>FreelancR</Card.Title>
+                            <Card.Text>A website that two different user types, freelancers and clients, can connect to expedite projects and quicly earn chops as a dev.</Card.Text>
+                        <Button variant="primary" href="https://github.com/ydefrawi/FreelancR">GitHub Repo</Button>
+                        <Button variant="primary" href="https://freelancr-excellence.herokuapp.com/">Launch Application!</Button>
+                        </Card.Body>
+
+                </Card>
+            </Container>
+
+            <Container className="d-flex justify-content-center" fluid>
+                <Row>
+                    <Card className="mb-3" style={{ color: "#000" }} style={{ width: '18rem' }}>
+                        <Card.Img src="https://picsum.photos/200/100" />
+                        <Card.Body>
+                        <Card.Title>New2Town</Card.Title>
+                        <Card.Text>An application that combines two APIs to make travelling and exploring a new city fun and easy.</Card.Text>
+                        <Button variant="primary" href="https://github.com/rutherford87/NewInTown">GitHub Repo</Button>
+                        <Button variant="primary">Website coming soon!</Button>
+                    </Card.Body>
+                    </Card>
+                    <Card className="mb-3" style={{ color: "#000" }} style={{ width: '18rem' }}>
+                        <Card.Img src="https://picsum.photos/200/100" />
+                        <Card.Body>
+                        <Card.Title>My first portfolio</Card.Title>
+                        <Card.Text>See how far I've come as a developer</Card.Text>
+                        <Button variant="primary" href="https://github.com/rutherford87/PROfolio_TPR">GitHub Repo</Button>
+                        <Button variant="primary">Website coming soon!</Button>
+                    </Card.Body>
+                    </Card>
+                </Row>
+            </Container>
+            <Container className="d-flex justify-content-center" fluid>
+                <Row>
+                    <Card className="mb-3" style={{ color: "#000" }} style={{ width: '18rem' }}>
+                        <Card.Img src="https://picsum.photos/200/100" />
+                        <Card.Body>
+                            <Card.Title>Test</Card.Title>
+                        </Card.Body>
+                    </Card>
+                    <Card className="mb-3" style={{ color: "#000" }} style={{ width: '18rem' }}>
+                        <Card.Img src="https://picsum.photos/200/100" />
+                        <Card.Body>
+                            <Card.Title>Test</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Row>
+            </Container>
+
+        </div>
+    );
+}
 
 export default Search;
